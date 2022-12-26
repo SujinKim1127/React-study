1553022d0dfe6433c2b7af5a1ba3707fe3f7e605
SuJin <1127sujink@naver.com>
2022-12-26 18:07:33 +0900

    TIL : connect & class

--- Redux toolkit/redux_toolkit2/README.md
+++ Redux toolkit/redux_toolkit2/README.md
@@ -39,3 +39,39 @@ import { useSelector, useDispatch } from "react-redux";
 - 매번 최신의 데이터를 받는다
 
 <br>
+
+### `connect` 와 클래스
+
+```jsx
+    incrementHandler() {}
+
+    decrementHandler() {}
+
+    toggleCounterHandler() {}
+
+    render() {
+        return (
+            <main className="classes.counter">
+                <h1>Redux Counter</h1>
+                <h3 className="classes.value">{counter}</h3>
+                <div>
+                    <button onClick={this.incrementHandler}>Increment</button>
+                    <button onClick={this.decrementHandler}>decrement</button>
+                </div>
+                <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+            </main>
+        )
+    
+    }
+```
+`this.incrementHandler` 처럼 `this`를 넣어줘서 제대로 Hook 하기
+
+
+```jsx
+import { connect } from "react-redux";
+```
+`connect` : 클래스 기반 컴포넌트를 리덕스에 연결하는 것을 도와준다. 함수에서도 사용이 가능하지만, 함수는 `useDispatch`를 사용하는 것이 더 좋다.
+
+`export default connect()(Counter)`
+`connect`가 실행되면 새 함수를 value로 리턴 -> 그럼 다시 실행 -> 컴포넌트를 보냄(변수로 리턴된 함수로)
+커넥트는 고차원 컴포넌트
--- Redux toolkit/redux_toolkit2/src/components/Counter.js
+++ Redux toolkit/redux_toolkit2/src/components/Counter.js
@@ -1,37 +1,78 @@
-import { useSelector, useDispatch } from "react-redux";
+import { Component } from "react";
+import { useSelector, useDispatch, connect } from "react-redux";
 // useStore: 저장소에 직접 액세스 가능
 // useSelector: 사용하기 더 편리. 저장소가 관리하는 상태 부분을 우리가 자동으로 선택 가능
 
 // 함수형 컴포넌트를 사용하지 않을 경우 `connect` 사용
 
-const Counter = () => {
-    const dispatch = useDispatch();         // redux store에 대한 action을 보낸다.
-    const counter = useSelector(state => state.counter); // 저장소가 관리하는 데이터에 액세스
-    // 함수는 `react-redux`가 실행
-    // 매번 최신의 데이터를 받는다
+// const Counter = () => {
+//     const dispatch = useDispatch();         // redux store에 대한 action을 보낸다.
+//     const counter = useSelector(state => state.counter); // 저장소가 관리하는 데이터에 액세스
+//     // 함수는 `react-redux`가 실행
+//     // 매번 최신의 데이터를 받는다
 
-    const incrementHandler = () => {
-        dispatch({ type: 'increment' })
+//     const incrementHandler = () => {
+//         dispatch({ type: 'increment' })
+//     }
+
+//     const decrementHandler = () => {
+//         dispatch({ type: 'decrement' });
+
+//     }
+
+//     const toggleCounterHandler = () => {};
+
+//     return (
+//         <main className="classes.counter">
+//             <h1>Redux Counter</h1>
+//             <h3 className="classes.value">{counter}</h3>
+//             <div>
+//                 <button onClick={incrementHandler}>Increment</button>
+//                 <button onClick={decrementHandler}>decrement</button>
+//             </div>
+//             <button onClick={toggleCounterHandler}>Toggle Counter</button>
+//         </main>
+//     )
+// }
+
+class Counter extends Component {
+    incrementHandler() {
+        this.props.increment();
     }
 
-    const decrementHandler = () => {
-        dispatch({ type: 'decrement' });
-
+    decrementHandler() {
+        this.props.decrement();
     }
 
-    const toggleCounterHandler = () => {};
+    toggleCounterHandler() {}
 
-    return (
-        <main className="classes.counter">
-            <h1>Redux Counter</h1>
-            <h3 className="classes.value">{counter}</h3>
-            <div>
-                <button onClick={incrementHandler}>Increment</button>
-                <button onClick={decrementHandler}>decrement</button>
-            </div>
-            <button onClick={toggleCounterHandler}>Toggle Counter</button>
-        </main>
-    )
+    render() {
+        return (
+            <main className="classes.counter">
+                <h1>Redux Counter</h1>
+                <h3 className="classes.value">{this.props.counter}</h3>
+                <div>
+                    <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+                    <button onClick={this.decrementHandler.bind(this)}>decrement</button>
+                </div>
+                <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+            </main>
+        )
+    
+    }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+    return {
+        counter: state.counter
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        increment: () => dispatch({ type: 'increment' }),
+        decrement: () => dispatch({ type: 'decrement' }),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
