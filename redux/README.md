36000c9d4d559ada95f6ede05b735ae92813da9e
SuJin <1127sujink@naver.com>
2023-01-22 13:45:51 +0900

    [TIL] : redux

--- redux/README.md
+++ redux/README.md
@@ -49,3 +49,88 @@ function Left3(props) {
 Left3: 1의 값을 바꾸기 위해 Right3 -> 2 -> 1-> Root -> left1 -> 2 -> 3 을 거쳐야만 한다.
 
 Right3과 Left3을 무선으로 연결시켜주는 것이 **Redux**
+
+<br> 
+
+```jsx
+import { Provider, useSelector, useDispatch } from "react-redux";
+```
+**Provider**: 사용한 컴포넌트 가장바깥쪽에 선언
+```jsx
+<Provider store={store}>
+    {/* Left1과 Right1은 store 사용 가능 */}
+    <Left1></Left1>
+    <Right1></Right1>
+</Provider>
+```
+
+<br>
+
+**useSelector** : number값을 Left3에서 표시하기 위해 사용
+함수를 입력받음 (이때, 화살표 함수도 가능하다)
+```jsx
+function Left3(props) {
+  function f(state) {
+      // state의 어떤값을 사용할것인지 작성
+    return state.number;
+  }
+
+  const number = useSelector(f);
+    // -- 또는 --
+  const number = useSelector((state)=>state.number);
+
+  return (
+    <div>
+      <h1>Left3 : {number}</h1>
+    </div>
+  );
+}
+```
+
+<br>
+
+**useDispatch** : number값을 바꾸기 위해 사용
+```jsx
+function Right3(props) {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>Right3</h1>
+      <input type="button" value="+"
+      onClick={() => {
+        dispatch({type: "PLUS"}) 
+      }}  
+      ></input>
+    </div>
+  );
+}
+```
+`dispatch`에 입력한 type action을 전달하여 `reducer`를 호출한다.
+
+
+<br>
+
+**reducer** : store의 state를 어떻게 바꿀것인지 결정하는 함수
+```jsx
+function reducer(currentState, action) {
+  if (currentState === undefined) {
+    // state가 정의되지 않으면
+    return {
+      number: 1, // 기본값은 1이다.
+    };
+  }
+
+  // 과거의 state를 복제하는 것
+  const newState = { ...currentState };
+
+  if(action.type === 'PLUS'){
+    newState.number++;
+  }
+  
+  // return 값은 새로운 state 값
+  return newState;
+}
+```
+`currentState`: 현재 state 값
+
+`action` : 어떻게 바꿀것인지에 대한 요청
--- redux/src/App.js
+++ redux/src/App.js
@@ -1,17 +1,43 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
+import { createStore } from "redux";
+import { Provider, useSelector, useDispatch } from "react-redux";
+// Provider: 사용한 컴포넌트 가장 바깥쪽에
+// useSelector: number갑을 Left3에서 표시하기 위해 사용
+// reducer: store의 state를 어떻게 바꿀것인지 결정
+function reducer(currentState, action) {
+  // 현재 state값    어떻게 바꿀것인지 요청
+
+  if (currentState === undefined) {
+    // state가 정의되지 않으면
+    return {
+      number: 1, // 기본값은 1이다.
+    };
+  }
+
+  // 과거의 state를 복제하는 것
+  const newState = { ...currentState };
+
+  if(action.type === 'PLUS'){
+    newState.number++;
+  }
+  // return 값은 새로운 state 값
+  return newState;
+}
+
+// 변경하면 안되기 때문에 const로 선언
+const store = createStore(reducer);
 
 export default function App() {
-  const [number, setNumber] = useState(1);
-
   return (
     <div id="container">
-      <h1>Root : {number}</h1>
+      <h1>Root</h1>
       <div id="grid">
-        <Left1 number={number}></Left1>
-        <Right1 onIncrease={() => {
-          setNumber(number+1);
-      }}></Right1>
+        <Provider store={store}>
+          {/* Left1과 Right1은 store 사용 가능 */}
+          <Left1></Left1>
+          <Right1></Right1>
+        </Provider>
       </div>
     </div>
   );
@@ -20,8 +46,8 @@ export default function App() {
 function Left1(props) {
   return (
     <div>
-      <h1>Left1 : {props.number}</h1>
-      <Left2 number={props.number}></Left2>
+      <h1>Left1 : </h1>
+      <Left2></Left2>
     </div>
   );
 }
@@ -29,16 +55,26 @@ function Left1(props) {
 function Left2(props) {
   return (
     <div>
-      <h1>Left2 : {props.number}</h1>
-      <Left3 number={props.number}></Left3>
+      <h1>Left2 : </h1>
+      <Left3></Left3>
     </div>
   );
 }
 
 function Left3(props) {
+  function f(state) {
+      // state의 어떤값을 사용할것인지 작성
+    return state.number;
+  }
+
+  // const number = useSelector(f);
+  const number = useSelector((state)=>state.number);
+  // useSelector는 함수를 입력받는다.
+  // 화살표 함수도 가능
+
   return (
     <div>
-      <h1>Left3 : {props.number}</h1>
+      <h1>Left3 : {number}</h1>
     </div>
   );
 }
@@ -47,9 +83,7 @@ function Right1(props) {
   return (
     <div>
       <h1>Right1</h1>
-      <Right2 onIncrease={() => {
-        props.onIncrease();
-      }}></Right2>
+      <Right2></Right2>
     </div>
   );
 }
@@ -58,24 +92,21 @@ function Right2(props) {
   return (
     <div>
       <h1>Right2</h1>
-      <Right3 onIncrease={() => {
-        props.onIncrease();
-      }}>
-      </Right3>
+      <Right3></Right3>
     </div>
   );
 }
 
 function Right3(props) {
+  // number값을 바꾸기 위해 dispatch 사용
+  const dispatch = useDispatch();
   return (
     <div>
       <h1>Right3</h1>
-      <input
-        type="button"
-        value="+"
-        onClick={() => {
-          props.onIncrease();
-        }}
+      <input type="button" value="+"
+      onClick={() => {
+        dispatch({type: "PLUS"})    // plus action 전달 -> reducer 호출됨
+      }}  
       ></input>
     </div>
   );
